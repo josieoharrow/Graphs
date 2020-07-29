3d78316e27b1c8a587e04611dedf841bd5ba70d0
Josie O'Harrow <josieoharrow@Josies-MacBook-Air-132.local>
2020-07-29 13:55:13 -0700

    finish scc finder yay

--- scc-rust/src/main.rs
+++ scc-rust/src/main.rs
@@ -9,13 +9,14 @@ fn main() {
 
     let contents = fs::read_to_string("../Reductions.csv")
         .expect("Something went wrong reading the file");
-    let row_array = contents.split("/n");
 
+    let row_array: Vec<&str> = contents.split("\n").collect();
     let mut g = DiGraph::<String, String>::new();
 
     let mut row_count = 1;
     for row in row_array {
-        let columns = row.split(",");
+        let columns: Vec<&str> = row.split(",").collect();
+        let columns_length = columns.len();
         let mut col_count = 1;
         for col in columns {
             if row_count == 1 {
@@ -24,22 +25,24 @@ fn main() {
             let row_index = g.node_indices().find(|i| g[*i] == row_count.to_string()).unwrap();
             let col_index = g.node_indices().find(|i| g[*i] == col_count.to_string()).unwrap();
 
-            if col == "1" {
-                g.add_edge(row_index, col_index, 1.);
+            if col.trim() == "1" {
+                g.add_edge(row_index, col_index, format!("{}{}", row_count.to_string(), col_count.to_string()));//(row_count).to_string());
             }
             col_count += 1;
         }
         row_count += 1;
     }
-    let num_nodes = 21;
 
+    let num_nodes = 21;
 
     let mut scc = tarjan_scc(&g);
     let mut popVal = scc.pop();
     while popVal != None {
         let unwrapped = popVal.unwrap();
-        println!("{}", unwrapped.len().to_string());
         if num_nodes == unwrapped.len() {
+            for i in unwrapped {
+                println!("{}", g[i]);
+            }
             println!("Found SCC!");
         }
         popVal = scc.pop();
