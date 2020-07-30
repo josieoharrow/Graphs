570061ed13392e38e267f21138e32a0453abd633
Josie O'Harrow <josieoharrow@hotmail.com>
2020-07-30 09:22:09 -0700

    remove gen files

--- scc-rust/target/debug/deps/scc_rust-2adda7bb71a2aa1f.d
+++
@@ -1,5 +0,0 @@
-/Users/josieoharrow/Desktop/Graphs/scc-rust/target/debug/deps/scc_rust-2adda7bb71a2aa1f: src/main.rs
-
-/Users/josieoharrow/Desktop/Graphs/scc-rust/target/debug/deps/scc_rust-2adda7bb71a2aa1f.d: src/main.rs
-
-src/main.rs:
--- scc-rust/target/debug/scc-rust.d
+++
@@ -1,1 +0,0 @@
-/Users/josieoharrow/Desktop/Graphs/scc-rust/target/debug/scc-rust: /Users/josieoharrow/Desktop/Graphs/scc-rust/src/main.rs
